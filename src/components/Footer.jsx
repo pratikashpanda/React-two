7c7d4c339a6ad5acaacfe7224a3caec65d078225
pratikash <pratikashpanda.1234@gmail.com>
2023-05-14 19:10:04 +0530

    build footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,71 @@
-import React from 'react'
-import {Box} from '@chakra-ui/react'
+import React from 'react';
+import {
+  Box,
+  Stack,
+  VStack,
+  Heading,
+  HStack,
+  Button,
+  Input,
+  Text
+} from '@chakra-ui/react';
+import { AiOutlineSend } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <Box bgColor={'blackAlpha.900'} minH={'40'}>
+    <Box bgColor={'blackAlpha.900'} minH={'40'} p={'16'} color={'white'}>
+      <Stack direction={['column', 'row']}>
+        <VStack alignItems={'stretch'} w={'full'} px={'4'}>
+          <Heading size={'md'} textTransform={'uppercase'} textAlign={['center', 'left']}>
+            Subscribe to get updates
+          </Heading>
+          <HStack borderBottom={'2px solid white'} py={'2'}>
+            <Input
+              placeholder="Enter your Email here..."
+              border={'none'}
+              borderRadius={'none'}
+              outline={'none'}
+              focusBorderColor="none"
+            />
+            <Button
+              p={'0'}
+              colorScheme="purple"
+              variant={'ghost'}
+              borderRadius={'0 20px 20px 0'}
+            >
+              <AiOutlineSend size={'20'} />
+            </Button>
+          </HStack>
+        </VStack>
 
+        <VStack
+          w={'full'}
+          borderLeft={['none', '1px solid white']}
+          borderRight={['none', '1px solid white']}
+        >
+          <Heading textTransform={'uppercase'} textAlign={'center'}>
+            Video hub
+          </Heading>
+          <Text>All Rights Reserved</Text>
+        </VStack>
+
+        <VStack w={'full'}>
+          <Heading size={'md'} textTransform={'uppercase'}>
+            Social Media
+          </Heading>
+          <Button variant={'link'} colorScheme={'purple'}>
+            <a href="https://www.youtube.com">YouTube</a>
+          </Button>
+          <Button variant={'link'} colorScheme={'purple'}>
+            <a href="https://www.instagram.com">Instagram</a>
+          </Button>
+          <Button variant={'link'} colorScheme={'purple'}>
+            <a href="https://www.github.com">Github</a>
+          </Button>
+        </VStack>
+      </Stack>
     </Box>
-  )
-}
+  );
+};
 
 export default Footer;
